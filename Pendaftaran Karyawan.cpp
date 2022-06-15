975eeb1068b352803ccc59cdc5946132a38a8cad
Kristiayuni24 <92391784+Kristiayuni24@users.noreply.github.com>
2022-06-15 15:26:08 +0700

    Update Pendaftaran Karyawan.cpp

--- Pendaftaran Karyawan.cpp
+++ Pendaftaran Karyawan.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include <queue>
 #include <stack>
-#include <conio.h>
-#include <stdlib.h>
+
 
 using namespace std;
 
 int main(){
+  stack <string> tumpukan; //deklarasi variabel
+  int pilih;
+  string input;
+ 
+do{
+  cout <<"=================================================" <<endl; 
+  cout <<"======Program Pendaftaraan Karyawan======" << endl;
+  cout <<" 1. Masukan data karyawan" << endl;
+  cout <<" 2. Menghapus data" << endl;
+  cout <<" 3. Mengecek data karyawan" <<endl;
+  cout <<" 4. Hapus "<<endl;
+  cout <<" 5. keluar Program" <<endl;
+  cout <<"=================================================" <<endl;
   
+  cout <<"Masukan Pilihan : ";
+  cin >> pilih;
 }
