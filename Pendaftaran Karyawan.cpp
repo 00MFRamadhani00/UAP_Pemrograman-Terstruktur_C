f202f0e78381d03eadcda4cdade2c293cbbe2374
Muhammad Fadhilah Ramadhani <92555706+00MFRamadhani00@users.noreply.github.com>
2022-06-15 17:44:16 +0700

    Update Pendaftaran Karyawan.cpp

--- Pendaftaran Karyawan.cpp
+++ Pendaftaran Karyawan.cpp
@@ -1,81 +1,87 @@
 #include <iostream>
-#include <stack>
-
+#include <conio.h>
+#include <windows.h>
+#include <queue>
 
 using namespace std;
 
 int main(){
-  stack <string> tumpukan; //deklarasi variabel
-  int pilih;
-  string input;
- 
-do{
-  cout <<"=================================================" <<endl; 
-  cout <<"        Program Pendaftaran Karyawan        " << endl;
-  cout <<" 1. Masukan data karyawan" << endl;
-  cout <<" 2. Menghapus data" << endl;
-  cout <<" 3. Mengecek data karyawan" <<endl;
-  cout <<" 4. Hapus "<<endl;
-  cout <<" 5. keluar Program" <<endl;
-  cout <<"=================================================" <<endl;
-  
-  cout <<"Masukan Pilihan : ";
-  cin >> pilih;
-  
-  switch (pilih){		
-	case 1:
-		cout<<"============================================"<<endl;
-		cout<<" Masukan data  :" ;
-		cin >> input;	
-		tumpukan.push(input);
-		system("CLS");
-		cout <<endl;
-		break;
-    
-        case 2:
-		if (tumpukan.empty()){		
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
-			cout <<"             Data sudah terhapus"<<endl ;
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
-		}else{
-			cout <<"Data karyawan ["<<tumpukan.top() <<"] telah di hapus" <<endl;
-			tumpukan.pop();
-		}
-		cout <<endl;
-		break;
-		  
-	case 3:
-		if(tumpukan.empty()){			//kondisi jika data masih kosong
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-			cout <<"Data tidak ditemukan"<<endl;
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		}else{
-			cout <<"Data karyawan tersimpan didata D : " <<endl;
-			while (!tumpukan.empty()){
-			cout <<"["<<tumpukan.top() <<"]"<<endl;		//menampilkan tumpukan baju
-			tumpukan.pop();
-		}
-		}
-		break;
-			
-	case 4:
-		system ("CLS");
-		break;
-			
-			
-	case 5:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Program Selesai"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		break;
-			
-	default:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Pilihan yang dimasukkan tidak tersedia"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		}
+	queue <string> antrian;
+	int pilih;
+	string data;
 	
-}while (pilih >=1 && pilih <=3);
-	   return 0;
+	do{
+		cout <<"=================================================" <<endl 
+			 <<"======Program Pendaftaraan Karyawan======" << endl
+			 <<" 1. Masukkan antrian karyawan" << endl
+			 <<" 2. Selesaikan antrian terdepan" << endl
+			 <<" 3. Kosongkan antrian" <<endl
+			 <<" 4. Mengecek antrian pendaftaran"<<endl
+			 <<" 5. Keluar" <<endl
+			 <<"=================================================" <<endl;
+			 
+		cout<<"Menu Yang Dipilih: ";cin>>pilih;
+		cin.ignore(256, '\n');
+		
+	if(pilih<1||pilih>5){
+		cout<<"Pilih Menu Lain";
+		Sleep(1500);
+	}else{
+		switch(pilih){
+			case 1:
+				cout<<"Masukkan: ";
+				getline(cin, data);
+				antrian.push(data); 
+				cout << "Data " << data << " Telah Dimasukkan";
+				Sleep(1500); 
+				system("CLS");
+				break;
+				
+			case 2:
+				if(antrian.empty()){
+					cout<<"Antrian Kosong";
+				}else{
+					antrian.pop();
+					cout << "Antrian terdepan telah selesai";
+				}
+				Sleep(1500); 
+				system("CLS");
+				break;
+				
+			case 3:
+				if(antrian.empty()){
+					cout<<"Antian Kosong";
+				}else{
+					antrian.empty();
+					cout << "Antrian dikosongkan!";
+				}
+				Sleep(1500); 
+				system("CLS");
+				break;
+				
+			case 4:
+				if(antrian.empty()){		
+					cout <<"Data kosong\n";		
+				}else{
+					cout <<"Antrian Data : ";
+					while (!antrian.empty()){
+						cout <<"\t"<<antrian.front() <<"\n\t";	
+						antrian.pop();
+					}
+				}
+				Sleep(5000); 
+				system("CLS");
+				break;
+				
+			case 5:
+				getch();
+				break;
+				
+			}			
+		}	
+	system("CLS");
+	}while (pilih != 5);
+	
+	cout << "Antrian ditutup";
+	return 0;
 }
-
