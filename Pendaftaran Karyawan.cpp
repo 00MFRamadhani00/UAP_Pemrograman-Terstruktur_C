a63358b6da8d2e64a27863d9e510a87f28aac210
Kristiayuni24 <92391784+Kristiayuni24@users.noreply.github.com>
2022-06-15 22:01:35 +0700

    Update Pendaftaran Karyawan.cpp

--- Pendaftaran Karyawan.cpp
+++ Pendaftaran Karyawan.cpp
@@ -12,7 +12,7 @@ int main(){
 	
 	do{
 		    cout <<"=================================================" <<endl 
-			 <<"       Program Pendaftaraan Karyawan      " << endl
+			 <<"       Program Pendaftaran Karyawan      " << endl
 			 <<"=================================================" <<endl   
 			 <<" 1. Masukkan nama pendaftar" << endl
 			 <<" 2. Selesaikan antrian terdepan" << endl
