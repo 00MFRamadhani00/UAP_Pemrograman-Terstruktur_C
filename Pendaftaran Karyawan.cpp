82c6dce1c1c9f71cb2d2374afeb65a93a4a6f938
Kristiayuni24 <92391784+Kristiayuni24@users.noreply.github.com>
2022-06-15 15:36:02 +0700

    Create Pendaftaran Karyawan.cpp

--- Pendaftaran Karyawan.cpp
+++ Pendaftaran Karyawan.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
-#include <stack>	//stl
+#include <stack>
+
+
 using namespace std;
 
 int main(){
-stack <string> tumpukan;	//deklarasi variabel
-int pilih;
-string input;
-
-do{		//perulangan do while
-cout <<"=================================================" <<endl; 
-cout <<"======Program Pendaftaraan Karyawan======" << endl;
-cout <<" 1. Masukan data karyawan" << endl;
-cout <<" 2. Menghapus data" << endl;
-cout <<" 3. Mengecek data karyawan" <<endl;
-cout <<" 4. Hapus "<<endl;
-cout <<" 5. keluar Program" <<endl;
-cout <<"=================================================" <<endl;
-	
-cout <<"Masukan Pilihan : ";
-cin >> pilih;
-	
-	switch (pilih){		//percabangan switch case
+  stack <string> tumpukan; //deklarasi variabel
+  int pilih;
+  string input;
+ 
+do{
+  cout <<"=================================================" <<endl; 
+  cout <<"        Program Pendaftaran Karyawan        " << endl;
+  cout <<" 1. Masukan data karyawan" << endl;
+  cout <<" 2. Menghapus data" << endl;
+  cout <<" 3. Mengecek data karyawan" <<endl;
+  cout <<" 4. Hapus "<<endl;
+  cout <<" 5. keluar Program" <<endl;
+  cout <<"=================================================" <<endl;
+  
+  cout <<"Masukan Pilihan : ";
+  cin >> pilih;
+  
+  switch (pilih){		
 	case 1:
 		cout<<"============================================"<<endl;
 		cout<<" Masukan data  :" ;
@@ -29,9 +31,9 @@ cin >> pilih;
 		system("CLS");
 		cout <<endl;
 		break;
-			
-	case 2:
-		if (tumpukan.empty()){		//kondisi jika data masih kosong
+    
+   case 2:
+		if (tumpukan.empty()){		
 			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
 			cout <<"             Data sudah terhapus"<<endl ;
 			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl ;
@@ -41,38 +43,4 @@ cin >> pilih;
 		}
 		cout <<endl;
 		break;
-			
-	case 3:
-		if(tumpukan.empty()){			//kondisi jika data masih kosong
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-			cout <<"Data tidak ditemukan"<<endl;
-			cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		}else{
-			cout <<"Data karyawan tersimpan didata D : " <<endl;
-			while (!tumpukan.empty()){
-			cout <<"["<<tumpukan.top() <<"]"<<endl;		//menampilkan tumpukan baju
-			tumpukan.pop();
-		}
-		}
-		break;
-			
-	case 4:
-		system ("CLS");
-		break;
-			
-			
-	case 5:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Program Selesai"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		break;
-			
-	default:
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		cout <<"Pilihan yang dimasukkan tidak tersedia"<<endl;
-		cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-		}
-	
-}while (pilih >=1 && pilih <=3);
-	   return 0;
 }
